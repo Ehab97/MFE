a23a42944117aea1fa145002aaeeefad6e84306f
Ehab97 <ehabreda04@gmail.com>
2024-10-11 17:04:17 +0300

    create the set up prod confgigration build

--- pakages/container/config/webpack.dev.js
+++ pakages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const { dependencies } = require("../package.json");
 const commonConfig = require("./webpack.common");
@@ -20,9 +20,7 @@ const devConfig = {
       },
       shared: dependencies,
     }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
+
   ],
 };
 
